4a116a96064dbc9b98ea7f9f1b0b10041422ea12
mahollis <mahollis@valdosta.edu>
2016-03-23 19:17:17 +0000

    Made small changes 3/23/16

--- app/views/groups/show.json.jbuilder
+++ app/views/groups/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @group, :id, :name, :description, :created_at, :updated_at
+json.extract! @group, :id, :title, :description, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322192700) do
+ActiveRecord::Schema.define(version: 20160322193852) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 20160322192700) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "memberships", ["group_id"], name: "index_memberships_on_group_id"
+  add_index "memberships", ["user_id"], name: "index_memberships_on_user_id"
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
