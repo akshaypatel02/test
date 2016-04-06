3618c1f95b907616bb56c21080f7b51aa352ce5f
mahollis <mahollis@valdosta.edu>
2016-04-06 21:10:20 +0000

    Work on CSS.

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   get 'home/leadCandidates'
   post 'home/promote'
   
-  root 'home#index', as: 'home'
+  root 'home#index', as: 'home' 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
